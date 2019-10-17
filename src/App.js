6b16a98d5d2f43defb673cd8acc6413a7240068e
Anderson <mfderson@gmail.com>
2019-10-17 18:55:42 -0300

    feature: importando imagem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import profile from './assets/avatar_professor.jpg';
+
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="200" src={profile} />;
 }
 
 export default App;
