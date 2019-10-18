4d9de4e634a38803081df6c5aedd717fa12720a0
Anderson <mfderson@gmail.com>
2019-10-17 21:33:01 -0300

    feature: ciclo de vida do componente

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,8 +5,27 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: ['Node.js', 'ReactJS', 'React Native']
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando um componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
